aec9632f54b1365467f44eec9a8060d313da1620
jcyepesgarcia <jcyepesgarcia@gmail.com>
2023-07-05 15:14:01 -0500

    delay 500, duration 3000

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ import "aos/dist/aos.css";
 
 AOS.init({
   offset: 120, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 2500, // values from 0 to 3000, with step 50ms
+  delay: 500, // values from 0 to 3000, with step 50ms
+  duration: 3000, // values from 0 to 3000, with step 50ms
   easing: "ease", // default easing for AOS animations
   once: false, // whether animation should happen only once - while scrolling down
   mirror: false, // whether elements should animate out while scrolling past them
